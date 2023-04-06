74c0cffa76d6c82c66a51eafbf3e78faaa61c57d
makwaje <alloushi2010@gmail.com>
2023-04-06 17:56:13 +0200

    set a ci/cd using git actions

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import "./directory.styles.scss";
-
 import CategoryItem from "../category-item/category-item.component";
+
 const Directory = ({ catagories }) => {
   return (
     <div className="directory-container">
